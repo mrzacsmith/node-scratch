4d4888c10744f59987e1a12409e7de3780fe6204
Zac Smith <zrs3141592@gmail.com>
2021-01-05 00:05:03 -0700

    set up config

---
+++ status-app/config.js
@@ -0,0 +1,25 @@
+// container for env info
+
+const enviroments = {}
+
+enviroments.staging = {
+  port: 3030,
+  envName: 'staging',
+}
+
+enviroments.production = {
+  port: 5000,
+  envName: 'production',
+}
+
+const currentEnviroment =
+  typeof process.env.NODE_ENV == 'string'
+    ? process.env.NODE_ENV.toLowerCase()
+    : ''
+
+const enviromentToExport =
+  typeof enviroments[currentEnviroment] == 'object'
+    ? enviroments[currentEnviroment]
+    : enviroments.staging
+
+module.exports = enviromentToExport
--- status-app/index.js
+++ status-app/index.js
@@ -1,7 +1,7 @@
-const { log } = require('console')
 const http = require('http')
 const url = require('url')
 const { StringDecoder } = require('string_decoder')
+const config = require('./config')
 
 const server = http.createServer((req, res) => {
   // 1: parse url
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
 
   // 2: get path
   let path = parsedUrl.pathname
-  let trimPath = path.replace(/^\/+|\/+$/g, '')
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
   // 6: query string as an object
   let queryStringObject = parsedUrl.query
@@ -28,24 +28,69 @@ const server = http.createServer((req, res) => {
   })
   req.on('end', () => {
     buffer += decoder.end()
+    // 9: call the hanlder for the request
+    let chosenHandler =
+      typeof router[trimmedPath] !== 'undefined'
+        ? router[trimmedPath]
+        : handlers.notFound
 
-    // 4: sent the response
-    res.end('node server\n')
+    // 10: construct the data object to send
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    }
 
-    // 5: log as needed
-    console.log('buffer:', buffer)
-    console.log('query', JSON.stringify(queryStringObject, null, 4))
+    // 11. route the request to router
+    chosenHandler(data, (statusCode, payload) => {
+      // default to 200
+      statusCode = typeof statusCode === 'number' ? statusCode : 200
+
+      // use payload or default to {}
+      payload = typeof payload === 'object' ? payload : {}
+
+      // convert to string
+      const payloadString = JSON.stringify(payload)
+
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+      // 5: log as needed
+      console.log('Return string payload', statusCode, payloadString)
+      // console.log('query', JSON.stringify(queryStringObject, null, 4))
+    })
   })
-
-  // 5: log the request path
-  // console.log(
-  //   `Request received on path: ${trimPath}\nwith method: ${method}\nwith query parameters:`
-  // )
-  // console.log(JSON.stringify(queryStringObject, null, 4))
-  // console.log('headers', headers)
 })
 
-const PORT = process.env.PORT || 3000
+// handlers
+const handlers = {}
+
+handlers.sample = (data, callback) => {
+  callback(406, { name: 'sample handler' })
+}
+
+let currentTime = new Date().toLocaleTimeString()
+handlers.status = (data, callback) => {
+  callback(200, {
+    currentTime,
+    message: 'Server is live',
+    author: 'Github @MrZacSmith',
+  })
+}
+
+handlers.notFound = (data, callback) => {
+  callback(404)
+}
+
+// Router
+const router = {
+  sample: handlers.sample,
+  status: handlers.status,
+}
+
+const PORT = config.port
 server.listen(PORT, () => {
-  console.log(`** Server is running on port ${PORT}`)
+  console.log(`** Server is running on port ${PORT} in ${config.envName} mode.`)
 })
