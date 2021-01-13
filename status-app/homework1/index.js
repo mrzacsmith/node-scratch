b7a9d2d9b8da486e491b1ff2314e66fd2e7a0275
Zac Smith <zrs3141592@gmail.com>
2021-01-13 00:24:02 -0700

    reset

--- status-app/homework1/index.js
+++ status-app/homework1/index.js
@@ -3,38 +3,34 @@ const http = require('http')
 const url = require('url')
 const config = require('./config.js')
 
-// create server and listener
-const server = http.createServer((req, res) => {
-  // get url and parse
-  const parsedUrl = url.parse(req.url, true)
+// // create server and listener
+// const server = http.createServer((req, res) => {
+//   // get url and parse
+//   const parsedUrl = url.parse(req.url, true)
 
-  // get the path
+//   // get the path
+//   const path = parsedUrl.pathname
+//   const reqPath = path.replace(/^\/+|\/+$/g, '')
+
+//   const method = req.method.toUpperCase()
+
+//   // did not request query strings, headers, or payload
+//   console.log('before end', method)
+
+//   // send the response by routing to router
+//   req.on('request', () => {
+//     console.log('hello')
+//   })
+// })
+
+const server = http.createServer()
+server.on('request', (req, res) => {
+  const parsedUrl = url.parse(req.url, true)
   const path = parsedUrl.pathname
   const reqPath = path.replace(/^\/+|\/+$/g, '')
+  console.log('request start', reqPath)
 
-  const method = req.method.toUpperCase()
-
-  // did not request query strings, headers, or payload
-
-  // send the response by routing to router
-  req.on('end', () => {
-    console.log('fired')
-    let routeHandler =
-      typeof router[reqPath] !== 'undefined'
-        ? router[reqPath]
-        : handlers.notFound
-    const data = {
-      reqPath,
-      method,
-    }
-    routeHandler =
-      (data,
-      (statusCode) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 200
-        res.setHeader('Content-Type', 'application/json')
-        res.writeHead(statusCode)
-      })
-  })
+  console.log('request end')
 })
 
 const PORT = config.port
