ec57825ea7df6d220691e09fc9a9bac1dd118541
Zac Smith <zrs3141592@gmail.com>
2021-01-13 00:40:46 -0700

    working but hacky

--- status-app/homework1/index.js
+++ status-app/homework1/index.js
@@ -3,55 +3,21 @@ const http = require('http')
 const url = require('url')
 const config = require('./config.js')
 
-// // create server and listener
-// const server = http.createServer((req, res) => {
-//   // get url and parse
-//   const parsedUrl = url.parse(req.url, true)
-
-//   // get the path
-//   const path = parsedUrl.pathname
-//   const reqPath = path.replace(/^\/+|\/+$/g, '')
-
-//   const method = req.method.toUpperCase()
-
-//   // did not request query strings, headers, or payload
-//   console.log('before end', method)
-
-//   // send the response by routing to router
-//   req.on('request', () => {
-//     console.log('hello')
-//   })
-// })
+const currentTime = new Date().toLocaleTimeString()
 
 const server = http.createServer()
 server.on('request', (req, res) => {
   const parsedUrl = url.parse(req.url, true)
   const path = parsedUrl.pathname
   const reqPath = path.replace(/^\/+|\/+$/g, '')
-  console.log('request start', reqPath)
 
-  console.log('request end')
+  if (reqPath === 'hello')
+    return res.end(`Hello Pirple from Node server at ${currentTime}`)
+
+  res.end('Not Found')
 })
 
 const PORT = config.port
 server.listen(PORT, () => {
   console.log(`** Server is listening on port ${PORT}. `)
 })
-
-// hello handler
-const handlers = {}
-
-const currentTime = new Date().toLocaleDateString()
-
-handlers.hello = (data, cb) => {
-  cb(200, { message: `Hello Pirple from Node server at ${currentTime}` })
-}
-
-handlers.notFound = (data, cb) => {
-  cb(404)
-}
-
-// hello router
-const router = {
-  hello: handlers.hello,
-}
